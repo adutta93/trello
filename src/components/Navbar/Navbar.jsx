862f270e98c2df4281078b007bfd9211b747e751
akash-dutta-au7 <dutta.akash1993official@gmail.com>
2021-10-17 19:11:26 +0530

    profile avatar added to navbar, style updated

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,9 +3,11 @@ import StarBorderIcon from "@mui/icons-material/StarBorder";
 import DateRangeIcon from "@mui/icons-material/DateRange";
 import Modal from "@mui/material/Modal";
 import Box from "@mui/material/Box";
+import { Avatar, AvatarGroup } from "@mui/material";
 
 import Popup from "../PopUp/Popup";
 
+import assignee from "../../utils/Assigne";
 import "./Nav.css";
 
 const style = {
@@ -26,13 +28,20 @@ const Navbar = ({ addList }) => {
   return (
     <div className="nav">
       <div className="logo">PROJECT MANAGEMENT</div>
+      <div className="avatar-grp">
+        <AvatarGroup max={5}>
+          {assignee.map((item, index) => (
+            <Avatar alt={item.name} src={item.img} />
+          ))}
+        </AvatarGroup>
+      </div>
       <nav className="nav-links">
         <ul>
-          <li className="star">
+          {/* <li className="star">
             <a href="#">
               <StarBorderIcon />
             </a>
-          </li>
+          </li> */}
           <li className="calender">
             <a href="#">Calender</a>
           </li>
--- src/utils/Assigne.js
+++ src/utils/Assigne.js
@@ -1,11 +1,17 @@
 import AionyHaust from "../assets/aiony-haust.jpg";
+import AlbertDera from "../assets/albert-dera.jpg";
+import ChristopherCampbell from "../assets/christopher-campbell.jpg";
+import IanDooley from "../assets/ian-dooley.jpg";
+import JosephGonzalez from "../assets/joseph-gonzalez.jpg";
+import MichaelDam from "../assets/michael-dam.jpg";
+
 const assignee = [
-  { name: "Aiony Haust", img: { AionyHaust } },
-  { name: "Albert Dera", img: { AionyHaust } },
-  { name: "Christopher Campbell", img: { AionyHaust } },
-  { name: "Ian Dooley", img: { AionyHaust } },
-  { name: "Joseph Gonzalez", img: { AionyHaust } },
-  { name: "Michael Dam", img: { AionyHaust } },
+  { name: "Aiony Haust", img: "https://unsplash.com/photos/iFgRcqHznqg" },
+  { name: "Albert Dera", img: { AlbertDera } },
+  { name: "Christopher Campbell", img: { ChristopherCampbell } },
+  { name: "Ian Dooley", img: { IanDooley } },
+  { name: "Joseph Gonzalez", img: { JosephGonzalez } },
+  { name: "Michael Dam", img: { MichaelDam } },
 ];
 
 export default assignee;
